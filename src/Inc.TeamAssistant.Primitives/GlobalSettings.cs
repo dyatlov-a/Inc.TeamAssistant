5e0fbc933657fd439ce3b7943280a1dc87b0873a
dyatlov-a <dyatlovall@gmail.com>
2024-07-15 23:00:36 +0300

    Rename team-assistant.online -> teamassist.bot

--- src/Inc.TeamAssistant.Primitives/GlobalSettings.cs
+++ src/Inc.TeamAssistant.Primitives/GlobalSettings.cs
@@ -2,7 +2,7 @@ namespace Inc.TeamAssistant.Primitives;
 
 public static class GlobalSettings
 {
-    public static readonly string CanonicalName = "team-assistant.online";
+    public static readonly string CanonicalName = "teamassist.bot";
     
     public static readonly string RequestDemoEmail = "dyatlovall@gmail.com";
 }
