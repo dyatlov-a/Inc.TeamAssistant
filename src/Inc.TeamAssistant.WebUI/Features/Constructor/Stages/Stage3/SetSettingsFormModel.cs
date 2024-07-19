26b5f084a0c2025c3baaca7f938f806ce74501ad
dyatlov-a <dyatlovall@gmail.com>
2024-07-20 00:39:02 +0300

    Add storage to constructor page

--- src/Inc.TeamAssistant.WebUI/Features/Constructor/Stages/Stage3/SetSettingsFormModel.cs
+++ src/Inc.TeamAssistant.WebUI/Features/Constructor/Stages/Stage3/SetSettingsFormModel.cs
@@ -5,17 +5,17 @@ namespace Inc.TeamAssistant.WebUI.Features.Constructor.Stages.Stage3;
 public sealed class SetSettingsFormModel
 {
     public IReadOnlyCollection<SelectListItem> Properties { get; set; } = Array.Empty<SelectListItem>();
-    
+
     public SetSettingsFormModel Apply(StagesState stagesState)
     {
         ArgumentNullException.ThrowIfNull(stagesState);
 
-        Properties = stagesState.PropertyKeys
-            .Select(v => new SelectListItem
+        Properties = stagesState.SelectedFeatures
+            .SelectMany(f => f.Properties.Select(v => new SelectListItem
             {
                 Name = v,
                 Value = stagesState.Properties.GetValueOrDefault(v, string.Empty)
-            })
+            }))
             .ToArray();
 
         return this;
