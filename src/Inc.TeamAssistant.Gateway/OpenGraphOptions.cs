1fe1c9435c7dff387af71396cce681abba9ee2ec
dyatlov-a <dyatlovall@gmail.com>
2024-07-25 19:07:40 +0300

    Fix problems on drawing open graph cards

--- src/Inc.TeamAssistant.Gateway/OpenGraphOptions.cs
+++ src/Inc.TeamAssistant.Gateway/OpenGraphOptions.cs
@@ -7,6 +7,8 @@ public sealed class OpenGraphOptions
     public string FontFamily { get; set; } = default!;
     public FontStyle FontStyle { get; set; }
     public float FontSize { get; set; }
+    public float LineSpacing { get; set; }
     public int Padding { get; set; }
     public string ImgFolder { get; set; } = default!;
+    public float BlendPercentage { get; set; }
 }
