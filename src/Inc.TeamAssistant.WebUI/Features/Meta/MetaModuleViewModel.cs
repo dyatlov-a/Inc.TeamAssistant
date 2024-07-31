f6867ae8730e2ac239e269ba8762191e66bda1ad
dyatlov-a <dyatlovall@gmail.com>
2024-08-01 00:53:05 +0300

    Refactoring MetaDataModule

--- src/Inc.TeamAssistant.WebUI/Features/Meta/MetaModuleViewModel.cs
+++
@@ -1,9 +0,0 @@
-using Inc.TeamAssistant.WebUI.Features.Common;
-
-namespace Inc.TeamAssistant.WebUI.Features.Meta;
-
-public sealed record MetaModuleViewModel(string Title)
-    : IViewModel<MetaModuleViewModel>
-{
-    public static MetaModuleViewModel Empty { get; } = new(string.Empty);
-}
