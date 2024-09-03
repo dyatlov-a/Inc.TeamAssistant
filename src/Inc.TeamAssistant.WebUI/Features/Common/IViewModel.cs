cd6ebe710f687583320ce353b3aabeff4c28df58
dyatlov-a <dyatlovall@gmail.com>
2024-09-03 22:39:25 +0300

    Refactoring bot constructor

--- src/Inc.TeamAssistant.WebUI/Features/Common/IViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Inc.TeamAssistant.WebUI.Features.Common;
-
-public interface IViewModel<out TViewModel>
-{
-    static virtual string PersistentKey { get; } = typeof(TViewModel).FullName!;
-    
-    static abstract TViewModel Empty { get; }
-}
--- src/Inc.TeamAssistant.WebUI/Features/Constructor/Stages/StagesPageViewModel.cs
+++
@@ -1,9 +0,0 @@
-using Inc.TeamAssistant.WebUI.Features.Common;
-
-namespace Inc.TeamAssistant.WebUI.Features.Constructor.Stages;
-
-public sealed record StagesPageViewModel(string Title)
-    : IViewModel<StagesPageViewModel>
-{
-    public static StagesPageViewModel Empty { get; } = new(string.Empty);
-}
