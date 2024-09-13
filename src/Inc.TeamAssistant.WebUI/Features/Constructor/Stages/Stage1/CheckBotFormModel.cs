acfc5748012b447e9269f20bd83a79f990b1a53a
dyatlov-a <dyatlovall@gmail.com>
2024-09-13 20:30:08 +0400

    Refactoring CheckBotFormTracker

--- src/Inc.TeamAssistant.WebUI/Features/Constructor/Stages/Stage1/CheckBotFormModel.cs
+++ src/Inc.TeamAssistant.WebUI/Features/Constructor/Stages/Stage1/CheckBotFormModel.cs
@@ -1,3 +1,5 @@
+using Inc.TeamAssistant.Constructor.Model.Queries.GetBotUserName;
+
 namespace Inc.TeamAssistant.WebUI.Features.Constructor.Stages.Stage1;
 
 public sealed class CheckBotFormModel
@@ -12,6 +14,24 @@ public sealed class CheckBotFormModel
         
         UserName = stagesState.UserName;
         Token = stagesState.Token;
+
+        return this;
+    }
+
+    public CheckBotFormModel Apply(GetBotUserNameResult checkResult)
+    {
+        ArgumentNullException.ThrowIfNull(checkResult);
+        
+        UserName = checkResult.UserName;
+        HasAccess = checkResult.HasAccess;
+
+        return this;
+    }
+
+    public CheckBotFormModel Clear()
+    {
+        UserName = string.Empty;
+        HasAccess = false;
 
         return this;
     }
