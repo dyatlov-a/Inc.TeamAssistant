d6c7ded5bd7576d3bfe1472072a7ecdce662831d
dyatlov-a <dyatlovall@gmail.com>
2024-10-02 00:52:44 +0400

    Added DateSelector component to story book

--- src/Inc.TeamAssistant.WebUI/Components/DateSelectorFactory.cs
+++ src/Inc.TeamAssistant.WebUI/Components/DateSelectorFactory.cs
@@ -11,35 +11,23 @@ internal sealed class DateSelectorFactory
         _resources = resources ?? throw new ArgumentNullException(nameof(resources));
     }
 
-    public IReadOnlyCollection<SelectItem<DateOnly>> CreateWeeks()
+    public IReadOnlyDictionary<string, DateOnly> CreateWeeks()
     {
-        return
-        [
-            new SelectItem<DateOnly>(
-                _resources[Messages.Dashboard_TwoWeeks],
-                DateOnly.FromDateTime(DateTimeOffset.UtcNow.AddDays(-14).Date)),
-            new SelectItem<DateOnly>(
-                _resources[Messages.Dashboard_FourWeeks],
-                DateOnly.FromDateTime(DateTimeOffset.UtcNow.AddDays(-28).Date)),
-            new SelectItem<DateOnly>(
-                _resources[Messages.Dashboard_TwelveWeeks],
-                DateOnly.FromDateTime(DateTimeOffset.UtcNow.AddDays(-84).Date))
-        ];
+        return new Dictionary<string, DateOnly>
+        {
+            [_resources[Messages.Dashboard_TwoWeeks]] = DateOnly.FromDateTime(DateTimeOffset.UtcNow.AddDays(-14).Date),
+            [_resources[Messages.Dashboard_FourWeeks]] = DateOnly.FromDateTime(DateTimeOffset.UtcNow.AddDays(-28).Date),
+            [_resources[Messages.Dashboard_TwelveWeeks]] = DateOnly.FromDateTime(DateTimeOffset.UtcNow.AddDays(-84).Date)
+        };
     }
     
-    public IReadOnlyCollection<SelectItem<DateOnly>> CreateMonths()
+    public IReadOnlyDictionary<string, DateOnly> CreateMonths()
     {
-        return
-        [
-            new SelectItem<DateOnly>(
-                _resources[Messages.Dashboard_OneMonth],
-                DateOnly.FromDateTime(DateTimeOffset.UtcNow.AddMonths(-1).Date)),
-            new SelectItem<DateOnly>(
-                _resources[Messages.Dashboard_ThreeMonths],
-                DateOnly.FromDateTime(DateTimeOffset.UtcNow.AddMonths(-3).Date)),
-            new SelectItem<DateOnly>(
-                _resources[Messages.Dashboard_SixMonths],
-                DateOnly.FromDateTime(DateTimeOffset.UtcNow.AddMonths(-6).Date))
-        ];
+        return new Dictionary<string, DateOnly>
+        {
+            [_resources[Messages.Dashboard_OneMonth]] = DateOnly.FromDateTime(DateTimeOffset.UtcNow.AddMonths(-1).Date),
+            [_resources[Messages.Dashboard_ThreeMonths]] = DateOnly.FromDateTime(DateTimeOffset.UtcNow.AddMonths(-3).Date),
+            [_resources[Messages.Dashboard_SixMonths]] = DateOnly.FromDateTime(DateTimeOffset.UtcNow.AddMonths(-6).Date)
+        };
     }
 }
