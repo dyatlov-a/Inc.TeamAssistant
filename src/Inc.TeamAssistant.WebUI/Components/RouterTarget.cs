08e4ecf5f19d7fe85905bc877dcae9565408d4b4
dyatlov-a <dyatlovall@gmail.com>
2024-10-03 17:33:28 +0400

    Added link component

---
+++ src/Inc.TeamAssistant.WebUI/Components/RouterTarget.cs
@@ -0,0 +1,9 @@
+namespace Inc.TeamAssistant.WebUI.Components;
+
+public static class RouterTarget
+{
+    public static readonly string Blank = "_blank";
+    public static readonly string Self = "_self";
+    public static readonly string Parent = "_parent";
+    public static readonly string Top = "_top";
+}
