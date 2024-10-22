ec6b8839446bc70d0b2aab616c5993995375e6fe
dyatlov-a <dyatlovall@gmail.com>
2024-10-22 19:30:18 +0300

    AssessmentSession added to StoryBook

--- src/Inc.TeamAssistant.Appraiser.Model/Common/StoryForEstimateDto.cs
+++ src/Inc.TeamAssistant.Appraiser.Model/Common/StoryForEstimateDto.cs
@@ -3,5 +3,6 @@ namespace Inc.TeamAssistant.Appraiser.Model.Common;
 public sealed record StoryForEstimateDto(
     long ParticipantId,
     string ParticipantName,
+    string AvatarUrl,
     string DisplayValue,
     int? ValueGroup);
