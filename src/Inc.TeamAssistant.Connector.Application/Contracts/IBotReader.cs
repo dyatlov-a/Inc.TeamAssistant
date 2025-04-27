2d5747bbfc482f94127e15f2e5efcd55a7089b92
dyatlov-a <dyatlovall@gmail.com>
2025-04-27 22:10:24 +0300

    Add link to dashboard for team owner

--- src/Inc.TeamAssistant.Connector.Application/Contracts/IBotReader.cs
+++ src/Inc.TeamAssistant.Connector.Application/Contracts/IBotReader.cs
@@ -8,6 +8,7 @@ public interface IBotReader
     Task<IReadOnlyCollection<Guid>> GetBotIds(CancellationToken token);
 
     Task<IReadOnlyCollection<BotDto>> GetBotsByUser(long userId, CancellationToken token);
+    
     Task<Guid?> FindBotId(long personId, CancellationToken token);
     
     Task<Bot?> Find(Guid id, DateTimeOffset now, CancellationToken token);
